b01f1b62690d73daab39b92d557f375c89f1d850
Wick2009 <sathwik.kintada@gmail.com>
2026-03-27 13:22:21 -0700

    more backend fixes

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ load_dotenv()
 # Setup of key Flask object (app)
 app = Flask(__name__)
 
-# Configure Flask Port, default to 8587 which is same as Docker setup
+# Configure Flask Port, default to 8424 which is same as Docker setup
 app.config['FLASK_PORT'] = int(os.environ.get('FLASK_PORT') or 8424)
 
 # Configure Flask to handle JSON with UTF-8 encoding versus default ASCII
